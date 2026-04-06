af8790146b7446868b7795668847f3391a2da36c
Harshcodes04 <harshitjantwal0418@gmail.com>
2026-04-06 21:16:00 +0530

    Added v1 models

---
+++ src/models/journalEntry.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const journalEntrySchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  mood: {
+    type: String,
+    enum: ["excited", "curious", "reflective", "sad", "happy"],
+  },
+  tags: [String],
+  linkedApod: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("JournalEntry", journalEntrySchema);
+

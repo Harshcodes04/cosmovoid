d2d15a5003a705933c21b2cf2bb9b63fd6e82048
Harshcodes04 <harshitjantwal0418@gmail.com>
2026-04-10 00:18:28 +0530

    feat(news): add news service and integrate with space controller

--- src/controller/spaceController.js
+++ src/controller/spaceController.js
@@ -1,5 +1,6 @@
 const nasaService = require("../services/nasa.service");
 const spacexService = require("../services/spacex.service");
+const newsservice = require("../services/news.service");
 
 exports.getApod = async (req, res, next) => {
   try {
@@ -12,7 +13,17 @@ exports.getApod = async (req, res, next) => {
 
 exports.getLatestLaunch = async (req, res) => {
   try {
-    const data = await spacexService.getLatestLaunchs();
+    const data = await spacexService.getLatestLaunch();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getLaunchById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = await spacexService.getLaunchById(id);
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -46,9 +57,29 @@ exports.getRockets = async (req, res) => {
   }
 };
 
+exports.getRocketById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = await spacexService.getRocketById(id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 exports.getCrew = async (req, res) => {
   try {
     const data = await spacexService.getCrew();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getCrewById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = await spacexService.getCrewById(id);
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -76,6 +107,37 @@ exports.getLandPads = async (req, res) => {
 exports.getRoadster = async (req, res) => {
   try {
     const data = await spacexService.getRoadster();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getAsteroids = async (req, res, next) => {
+  try {
+    const data = await nasaService.getAsteroids();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.searchNasaMedia = async (req, res, next) => {
+  try {
+    const { query, mediaType } = req.query;
+    if (!query) {
+      return res.status(400).json({ error: "Query parameter is required" });
+    }
+    const data = await nasaService.searchNasaMedia(query, mediaType);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getSpaceNews = async (req, res, next) => {
+  try {
+    const data = await newsservice.getSpaceNews();
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
---
+++ src/services/news.service.js
@@ -0,0 +1,20 @@
+const cache = require("../utils/cache");
+const axios = require("axios");
+
+exports.getSpaceNews = async () => {
+  const cacheKey = "space-news";
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) {
+    return cachedData;
+  }
+  try {
+    const res = await axios.get(
+      "https://api.spaceflightnewsapi.net/v4/articles/?limit=20",
+    );
+    cache.set(cacheKey, res.data, 60 * 60 * 1000);
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching space news", error.message);
+    throw error;
+  }
+};

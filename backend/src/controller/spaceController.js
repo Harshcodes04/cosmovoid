7416968eedb2ab737a992878a98a3c34f1750ab5
Harshcodes04 <harshitjantwal0418@gmail.com>
2026-05-27 00:16:26 +0530

    feat(space): implement dedicated controllers for astronomical data

--- backend/src/controller/spaceController.js
+++ backend/src/controller/spaceController.js
@@ -4,6 +4,7 @@ const launchLibraryService = require("../services/launchLibrary.service");
 const newsservice = require("../services/news.service");
 const asyncHandler = require("../utils/asyncHandler");
 const astronomyService = require("../services/astronomy.service");
+const skyEventsService = require("../services/skyEvents.service");
 
 
 exports.getApod = asyncHandler(async (req, res, next) => {
@@ -65,32 +66,17 @@ exports.getAstronautById = asyncHandler(async (req, res, next) => {
   res.json(data);
 });
 
-exports.getLaunchPads = asyncHandler(async (req, res, next) => {
-  const data = await spacexService.getLaunchPads();
-  res.json(data);
-});
-
-exports.getLandPads = asyncHandler(async (req, res, next) => {
-  const data = await spacexService.getLandPads();
-  res.json(data);
-});
-
-exports.getRoadster = asyncHandler(async (req, res, next) => {
-  const data = await spacexService.getRoadster();
-  res.json(data);
-});
-
 exports.getAsteroids = asyncHandler(async (req, res, next) => {
   const data = await nasaService.getAsteroids();
   res.json(data);
 });
 
 exports.searchNasaMedia = asyncHandler(async (req, res, next) => {
-  const { query, mediaType } = req.query;
+  const { query, mediaType, page } = req.query;
   if (!query) {
     return res.status(400).json({ error: "Query parameter is required" });
   }
-  const data = await nasaService.searchNasaMedia(query, mediaType);
+  const data = await nasaService.searchNasaMedia(query, mediaType, page);
   res.json(data);
 });
 
@@ -114,4 +100,9 @@ exports.getAstronomyToken = (req, res) => {
     `${process.env.ASTRONOMY_API_ID}:${process.env.ASTRONOMY_API_SECRET}`,
   ).toString("base64");
   res.json({ token: authString });
+};
+
+exports.getSkyEvents = (req, res) => {
+  const data = skyEventsService.getSkyEvents();
+  res.json(data);
 };
